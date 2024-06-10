b0ee9e36c2d80de4edd85e1331d85b685c3d7b59
em-au <146881865+em-au@users.noreply.github.com>
2024-06-10 00:36:00 +0000

    Add Create page and ability to check if username exists

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -2,7 +2,29 @@
 
 class Create extends Controller {
 
-    public function index() {		
-	    $this->view('create/index');
+  public function index() {		
+    $this->view('create/index');
+  }
+
+  public function createUser() { 
+    /*
+    1a. Will call function checkUsernameExists(username) in Model User
+    1b. if userExists sets session variable usernameExists to true, then send to View Login
+    1c. View Login will display error username taken if that session variable is true
+    2. this function will check if passwords match. if not, set a session variable and
+    direc to View Login (will display an error message)
+    3. this function will check if password is 8 char. if not, set a session variable and
+    direct to View Login (will display an error message)
+    */
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+
+
+    $user = $this->model('User');
+    $user->checkUsernameExists($username); 
+    // echo $_SESSION['test']; // for testing, yes this does call the fxn in User
+    if (isset($_SESSION['usernameExists']) && $_SESSION['usernameExists'] == true) {
+      echo "Username taken";
     }
+  }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -2,49 +2,68 @@
 
 class User {
 
-    public $username;
-    public $password;
-    public $auth = false;
+  public $username;
+  public $password;
+  public $auth = false;
 
-    public function __construct() {
-        
-    }
+  public function __construct() {
+      
+  }
 
-    public function test () {
-      $db = db_connect();
-      $statement = $db->prepare("select * from users;");
-      $statement->execute();
-      $rows = $statement->fetch(PDO::FETCH_ASSOC);
-      return $rows;
-    }
+  public function test () {
+    $db = db_connect();
+    $statement = $db->prepare("select * from users;");
+    $statement->execute();
+    $rows = $statement->fetch(PDO::FETCH_ASSOC);
+    return $rows;
+  }
 
-    public function authenticate($username, $password) {
-        /*
-         * if username and password good then
-         * $this->auth = true;
-         */
-		$username = strtolower($username);
-		$db = db_connect();
+  public function authenticate($username, $password) {
+      /*
+       * if username and password good then
+       * $this->auth = true;
+       */
+    $username = strtolower($username);
+    $db = db_connect();
         $statement = $db->prepare("select * from users WHERE username = :name;");
         $statement->bindValue(':name', $username);
         $statement->execute();
         $rows = $statement->fetch(PDO::FETCH_ASSOC);
-		
-		if (password_verify($password, $rows['password'])) {
-			$_SESSION['auth'] = 1;
-			$_SESSION['username'] = ucwords($username);
-			unset($_SESSION['failedAuth']);
-			header('Location: /home');
-			die;
-		} else {
-			if(isset($_SESSION['failedAuth'])) {
-				$_SESSION['failedAuth'] ++; //increment
-			} else {
-				$_SESSION['failedAuth'] = 1;
-			}
-			header('Location: /login');
-			die;
-		}
+    
+    if (password_verify($password, $rows['password'])) {
+      $_SESSION['auth'] = 1;
+      $_SESSION['username'] = ucwords($username);
+      unset($_SESSION['failedAuth']);
+      header('Location: /home'); // should this be done by the controller?
+      die;
+    } else {
+      if(isset($_SESSION['failedAuth'])) {
+        $_SESSION['failedAuth'] ++; //increment
+      } else {
+        $_SESSION['failedAuth'] = 1;
+      }
+      header('Location: /login'); // should this be done by the controller?
+      die;
     }
+  }
+
+  // Check if username exists in the database
+  // think i need to bind a value like in the authenticate fxn for security reasons 
+  public function checkUsernameExists($username) {
+    $_SESSION['test'] = 'test';
+    $db = db_connect();
+    $statement = $db->prepare("SELECT username FROM users WHERE username = '$username'");
+    $statement->execute(); 
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    
+    if (isset($row) && !empty($row)) {
+      $_SESSION['usernameExists'] = true;
+    }
+    else {
+      $_SESSION['usernameExists'] = false;
+    }
+    // die;
+  }
 
 }
