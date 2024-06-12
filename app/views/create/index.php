d7272f99487a7e8aaf63c238d21929a82a8e1579
em-au <146881865+em-au@users.noreply.github.com>
2024-06-12 02:32:40 +0000

    Center the login and sign up forms

--- app/views/create/index.php
+++ app/views/create/index.php
@@ -1,5 +1,5 @@
 <?php require_once 'app/views/templates/headerPublic.php'?>
-<main role="main" class="container">
+<main role="main" class="container" style="display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; min-height: 100vh;">
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
@@ -10,17 +10,17 @@
 
 <div class="row">
     <div class="col-sm-auto">
-        <form action="/create/create_user" method="post" >
+        <form action="/create/create_user" method="post" style="width: 300px;">
         <fieldset>
-            <div class="form-group">
+            <div class="form-group" style="text-align: left">
                 <label for="username">Username</label>
                 <input required type="text" class="form-control" name="username">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="text-align: left">
                 <label for="password">Password</label>
                 <input required type="password" class="form-control" name="password1">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="text-align: left">
                 <label for="password">Confirm Password</label>
                 <input required type="password" class="form-control" name="password2">
               </div>
--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,5 +1,5 @@
 <?php require_once 'app/views/templates/headerPublic.php'?>
-<main role="main" class="container">
+<main role="main" class="container" style="display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; min-height: 100vh;">
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
@@ -20,13 +20,13 @@
 
 <div class="row">
     <div class="col-sm-auto">
-		<form action="/login/verify" method="post" >
+		<form action="/login/verify" method="post" style="width: 300px">
 		<fieldset>
-			<div class="form-group">
+			<div class="form-group" style="text-align: left">
 				<label for="username">Username</label>
 				<input required type="text" class="form-control" name="username">
 			</div>
-			<div class="form-group">
+			<div class="form-group" style="text-align: left">
 				<label for="password">Password</label>
 				<input required type="password" class="form-control" name="password">
 			</div>
@@ -64,5 +64,5 @@
 		unset($_SESSION['locked']);
 	}
 ?>
-<br><br>
+<br>
 <?php require_once 'app/views/templates/footer.php' ?>
