e01b4d4ce47808c5b3746192b64dff11c954f5d0
em-au <146881865+em-au@users.noreply.github.com>
2024-06-14 03:21:29 +0000

    Change lockout time back to 60s

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -68,7 +68,7 @@ class Login extends Controller {
 				 // Call model to get time of last failed attempt
 				$log->lock_time($_SESSION['username']);
 				$_SESSION['lock_start'] = $log->time;
-				$_SESSION['lock_end'] = strtotime($_SESSION['lock_start']) + 5; // CHANGE BACK TO 60 SEC
+				$_SESSION['lock_end'] = strtotime($_SESSION['lock_start']) + 60; 
 			}
 		}
 
